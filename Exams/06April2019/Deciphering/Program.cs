5bff82e77da0dff6d314ece2b9e51b80b8b9c1f8
DImitar Dimitrov <32570431+Dimodim@users.noreply.github.com>
2019-04-11 18:45:56 +0300

    Added solutions

---
+++ Exams/06April2019/Deciphering/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Deciphering
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string decripted = "";
+            string[] replaceString = Console.ReadLine().Split();
+            Regex patern = new Regex (@"[d-z#|{}]");
+            if (patern.IsMatch(input))
+            {
+                foreach (var charecter in input)
+                {
+
+                    int charToNumber = charecter-3;
+
+                    char numToChar = (char)charToNumber;
+
+                    decripted += numToChar;
+                }
+                
+                if (decripted.Contains(replaceString[0]))
+                {
+                    decripted=decripted.Replace(replaceString[0], replaceString[1]);
+                }
+                Console.WriteLine(decripted);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+            
+        }
+    }
+}
---
+++ Exams/06April2019/Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, List<string>>();
+            var wordsAndDefinitions = Console.ReadLine().Split(" | ").ToArray();
+            for (int i = 0; i < wordsAndDefinitions.Length; i++)
+            {
+                string[] splited = wordsAndDefinitions[i].Split(": ").ToArray();
+                if (!dictionary.ContainsKey(splited[0]))
+                {
+                    dictionary.Add(splited[0], new List<string>());
+                }
+                dictionary[splited[0]].Add(splited[1]);
+            }
+            var wordsToPrint = Console.ReadLine().Split(" | ").ToArray();
+            var listOrEnd = Console.ReadLine();
+            if (listOrEnd == "End")
+            {
+                var print = new Dictionary<string, List<string>>();
+                foreach (var word in wordsToPrint)
+                {
+                    if (dictionary.ContainsKey(word))
+                    {
+                        var curentWordValue = dictionary[word];
+                        print[word] = curentWordValue;
+                    }
+                }
+                foreach (var word in print.OrderBy(x=>x.Key))
+                {
+                    Console.WriteLine(word.Key);
+                    foreach (var item in word.Value.OrderByDescending(x=>x.Length))
+                    {
+                        Console.WriteLine(" -"+item);
+                    }
+                }
+            }
+            else
+            {
+                var print = new List<string>();
+                foreach (var word in dictionary.OrderBy(x=>x.Key))
+                {
+                    print.Add(word.Key);
+                }
+                Console.WriteLine(string.Join(" ",print));
+            }
+            
+
+        }
+    }
+}
